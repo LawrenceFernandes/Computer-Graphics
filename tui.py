4b1f5a218e4f6d4a43e62ebb5cfd871a491c3f14
LawrenceFernandes <lawrence.stfs@gmail.com>
2016-09-15 16:58:35 -0300

    Update tui.py

--- tui.py
+++ tui.py
@@ -6,7 +6,7 @@
 # Copyright (C) 2016 Lawrence Fernandes
 
 """ This module is the terminal user interface (TUI) of the app.
-    It calls the other modules to draw the polygon requested by the user.
+    It calls the other modules to draw the polyhedron requested by the user.
     The other modules must be located on the same directory.
 """
 # Import Python modules
@@ -16,23 +16,28 @@ from time import gmtime, strftime
 def menu():
     print('\nUsage: %s [-options]' % sys.argv[0])
     print('where the options are:')
-    print('  -m    Draws the Polygon mesh (polyhedron with vertices, edges and faces).')
-    print('  -f    Draws the Polyhedron without its faces.')
+    print('  -m    Draws the polygon mesh (polyhedron with vertices, edges and faces).')
+    print('  -f    Draws the polyhedron without its faces, just vertices and edges.')
+    print('  -c    Grants the user keyboard control over the rotation of the polyhedron.')
 
 if __name__ == '__main__':
-    print('\nStarting Interactive Geometry 1.0 at %s' % strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+    print('\nStarting pylyhedron 1.0 at %s' % strftime("%Y-%m-%d %H:%M:%S", gmtime()))
     print('(c) 2016 Lawrence Fernandes')
     option = ' '.join(sys.argv[1:])
     Done = False
     while not Done:
         if option=="-m":
-            os.system("python cube_wfaces.py")
+            os.system("python cube.py -m")
             break
-            #print("Cube with face")
+        elif option == '-m -c':
+            os.system('python cube.py -m -c')
+            break
         elif option == '-f':
-            os.system('python cube_faceless.py')
+            os.system('python cube.py -f')
             break
-            #print("Faceless cube")
+        elif option == '-f -c':
+            os.system('python cube.py -f -c')
+            break    
         elif option == "Q":
             break
         else:
